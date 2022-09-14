18314bd5c668b03e867d4e5da35b6e796790f7c7
lamine2000 <lemzogueye.lg72@gmail.com>
2022-09-14 06:01:52 +0000

    Bson to POJO mapping setup

--- src/com/lamine/dao/Connection.java
+++ src/com/lamine/dao/Connection.java
@@ -1,24 +1,34 @@
 package com.lamine.dao;
 
+import com.mongodb.ConnectionString;
+import com.mongodb.MongoClientSettings;
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
 import org.bson.Document;
+import org.bson.codecs.configuration.CodecRegistry;
+import org.bson.codecs.pojo.PojoCodecProvider;
+
+import static org.bson.codecs.configuration.CodecRegistries.fromProviders;
+import static org.bson.codecs.configuration.CodecRegistries.fromRegistries;
 
 public class Connection {
-    private static final String connectionString = "mongodb://discordbot:pass@localhost:27017/?retryWrites=true&w=majority";
+    private static final ConnectionString connectionString = new ConnectionString("mongodb://discordbot:pass@localhost:27017/?retryWrites=true&w=majority");
 
     public static MongoDatabase getDiscordBotsDB() throws Exception {
         MongoDatabase discordBotsDB;
 
-        try{
-            MongoClient mongoClient = MongoClients.create(connectionString);
-            discordBotsDB = mongoClient.getDatabase("projectManagerBot");
-            return discordBotsDB;
-        }catch (Exception e){
-            throw new Exception("Connection to database failed");
-        }
+        CodecRegistry pojoCodecRegistry = fromProviders(PojoCodecProvider.builder().automatic(true).build());
+        CodecRegistry codecRegistry = fromRegistries(MongoClientSettings.getDefaultCodecRegistry(), pojoCodecRegistry);
+        MongoClientSettings clientSettings = MongoClientSettings.builder()
+                .applyConnectionString(connectionString)
+                .codecRegistry(codecRegistry)
+                .build();
+
+        MongoClient mongoClient = MongoClients.create(clientSettings);
+        discordBotsDB = mongoClient.getDatabase("projectManagerBot");
+        return discordBotsDB;
     }
 
     public static MongoCollection<Document> getCollectionFromDB(String collectionName) throws Exception {
